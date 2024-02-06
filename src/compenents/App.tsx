6d464750169b50a6e48bb39bd5be09f74c1b0a43
Edmund Prescott <78518690+Edmund-Prescott@users.noreply.github.com>
2024-02-05 19:12:19 -0800

    Routes, firebase credentials
    @types/bootstrap breaking production build (cringe)

---
+++ src/compenents/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Signup from "./Signup";
+import Login from "./Login";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
